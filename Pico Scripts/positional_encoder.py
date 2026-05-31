d85837acfd7feb7fb916b6cb70d47dc3916134e4
Jotham Hughes <jhughes266@outlook.com>
2026-06-01 05:51:23 +1000

    Added the positional encoder class that reads the voltages at the positional encoder pins and then transforms it into a decimal number that corresponds to the current position.

---
+++ Pico Scripts/positional_encoder.py
@@ -0,0 +1,42 @@
+from machine import Pin
+
+class PositionalEncoder:
+    """
+    This class is responsible for reading the values from a positional encoder
+    and changing this from a binary value into a decimal one.
+    """
+    def __init(self, bitPinPairList):
+        """
+        Inits for the PositionalEncoder class.
+        
+        Args:
+        bitPinList: List where the key is the bit number and the value is the
+        pin which corresponds to that bit.
+        """
+        # Save as a private attribute
+        self._bitPinPairList = bitPinPairList
+        
+        # Go through and convert all the values of the Listionary to Pin objects
+        for bitNumber in range(len(self._bitPinPairList)):
+            self._bitPinPairList[bitNumber] = Pin(self._bitPinPairList[bitNumber], Pin.IN)
+    
+    def getPosition(self)
+        """
+        Gets the current position of read by the positional encoder. Also converts
+        it into decimal
+        """
+        # Stores the decimal position
+        decimalPosition = 0
+        
+        # Loop through and read all the pins in the bitPinList accumulating the
+        # value in the "decimalPosition" variable.
+        for bitNumber, pin in enumerate(self._bitPinPairList):
+            decimalPosition += (2 ** bitNumber) * pin.value()
+        
+        return decimalPosition
+            
+            
+        
+        
+        
+
