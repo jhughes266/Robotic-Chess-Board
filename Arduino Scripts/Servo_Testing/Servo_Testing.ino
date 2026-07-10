4320097bd214f32436d7443ac0657c026a65d4a6
Jotham Hughes <jhughes266@outlook.com>
2026-07-10 23:52:17 +1000

    Testing the servos used for the piece mover (magnet gripper). Have found thier start and end locations I may need to slow down the movement of the upper one when it is pushing the magnet!

--- Arduino Scripts/Servo_Testing/Servo_Testing.ino
+++ Arduino Scripts/Servo_Testing/Servo_Testing.ino
@@ -1,12 +1,29 @@
 #include <Servo.h>
-Servo testServo;
-int pos = 0;
+Servo servoLower;
+Servo servoUpper;
+int lowerPos = 0;
+int upperPos = 0;
+
+int lowerStartPos = 95;
+int upperStartPos = 141;
+
+int lowerFinishPos = 20;
+int upperFinishPos = 110;
 void setup() {
   // put your setup code here, to run once:
-  testServo.attach(9);
+  servoLower.attach(9);
+  servoUpper.attach(10);
 }
 
 void loop() {
+  delay(1000);
+  servoLower.write(lowerStartPos);
+  servoUpper.write(upperStartPos);
+  delay(1000);
+  servoLower.write(lowerFinishPos);
+  delay(1000);
+  servoUpper.write(upperFinishPos);
+  /*
 for(pos = 175; pos >= 90; pos -= 1)
   {
     testServo.write(pos);
@@ -18,4 +35,5 @@ for(pos = 175; pos >= 90; pos -= 1)
     testServo.write(pos);
     delay(50);
   }
+  */
 }
