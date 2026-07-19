4b1b1ff8608fe1c148ef1ec58edcffc953e8ecc0
Jotham Hughes <jhughes266@outlook.com>
2026-07-20 07:41:59 +1000

    Started the piece path finding. Begun by laying out all the files I need to perform the best first search. This included a node, probllem, priority queue and best first search file. Also added all the attributes to the node file and added the board layout dictionary in the main file (this may change though dependent on memory and time constraints)

---
+++ Rpi Scripts/piece_path_finding/node.py
@@ -0,0 +1,17 @@
+
+class Node:
+    """
+    Node object that is used in the best first search algorithm.
+    """
+    def __init__(self, state, parent, action, pathCost):
+        """
+        Args:
+            state: The state of the graph associated with the node.
+            parent: The parent of this node. Once the goal is found the solution can trace back through its parents to get to the initial state and thus is finds a solution.
+            action: The action that got us to the state on this node.
+            pathCost: The CUMULATIVE path cost from the initial state to the current state.
+        """
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = pathCost
