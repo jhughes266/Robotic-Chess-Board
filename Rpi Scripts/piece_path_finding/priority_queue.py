4d923c60dd224ef06b5e5bdf92c75fe8aef6ae53
Jotham Hughes <jhughes266@outlook.com>
2026-07-21 04:29:14 +1000

    Created a priority queue class also created a config file fpr the piece path finding

--- Rpi Scripts/piece_path_finding/priority_queue.py
+++ Rpi Scripts/piece_path_finding/priority_queue.py
@@ -1,3 +1,23 @@
 
 class PriorityQueue:
-    pass
+
+    def __init__(self, sortingLambda=lambda x: x):
+        self.__elements = []
+        self.__sortingLambda = sortingLambda
+
+    def __len__(self):
+        return len(self.__elements)
+
+    def empty(self):
+        return len(self) == 0
+
+    def push(self, item):
+        self.__elements.append(item)
+        self.__elements.sort(key=self.__sortingLambda)
+
+    def pop(self):
+        return self.__elements.pop(0)
+
+    def top(self):
+        return self.__elements[0]
+
