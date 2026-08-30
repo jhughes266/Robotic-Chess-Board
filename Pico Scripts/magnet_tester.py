ec90fd0bf01c8db222838821a4fc41a524b20bb5
Jotham Hughes <jhughes266@outlook.com>
2026-08-31 02:16:31 +1000

    Added a testing script for the pico that will move the magnet around to random positions a speceified number of times. This is going to be used to test which magnets and board material should be used to reduce the number of times a magnet falls!

---
+++ Pico Scripts/magnet_tester.py
@@ -0,0 +1,48 @@
+from robot_object_config import pieceMover
+from command_executer import excuteCommand
+import random
+
+# Make the number of itterations 40
+def magnet_tester(numberOfIterations):
+    """
+    This function is designed to test how well different magnets and materials
+    on the board perform. It randomly moves a single magnet over the board to
+    different locations.
+    Args:
+        numberOfIterations: The number of testing itterations
+    """
+    # Where the magnet intially starts
+    initialMagnetLocation = [0,0]
+    # The current location of the magnet
+    currentMangetLocation = initialMagnetLocation[::]
+    # Disengage the gripper before the program starts
+    pieceMover.disengageGripper()
+    # Loop through the specified number of itterations
+    for iteration in range(1, numberOfIterations + 1):
+        # Move to the current location and engage the gripper
+        pieceMover.moveToGridXY(gridX=currentMagnetLocation[0], gridY=currentMagnetLocation[1])
+        pieceMover.engageGripper()
+        # Get a new location and move the magner there and then disengage the
+        # gripper.
+        newX = random.randint(0,11)
+        newy = random.randint(0,11)
+        pieceMover.moveToGridXY(gridX=newX, gridY=newY)
+        pieceMover.disengageGripper()
+        # Update the current location of the magnet
+        currentMagnetLocation[0] = newX
+        currentMagnetLocation[1] = newY
+        # This pauses the execution of the routine incase a magnet has fallen
+        haltFlag = input("Enter nothing to continue OR enter anything to halt")
+        
+        if haltFlag != "":
+            print(itteration)
+            break
+        
+    # Move the magnet back to the starting location
+    pieceMover.moveToGridXY(gridX=currentMagnetLocation[0], gridY=currentMagnetLocation[1])
+    pieceMover.engageGripper()
+    
+    pieceMover.moveToGridXY(gridX=initialMagnetLocation[0], gridY=initialMagnetLocation[1])
+    pieceMover.disengageGripper()
+    print("Program end!!!")
+
--- Rpi Scripts/testing.py
+++ Rpi Scripts/testing.py
@@ -1,3 +1,8 @@
+testInput = input("Enter input")
+if testInput == "":
+    print("Works")
+
+"""
 testStr = '[2,3]E[1,3]D[3,3]E[2,3]D[5,5]E[5,4]D[9,3]E[11,3]D[2,2]E[0,2]D[9,2]E[11,2]D[4,2]E[3,2]D[6,3]E[6,2]D[2,8]E[1,8]D[3,8]E[2,8]D[4,8]E[3,8]D[9,8]E[10,8]D[8,8]E[9,8]D[7,8]E[8,8]D[10,8]E[11,8]D[2,9]E[0,9]D[9,9]E[11,9]D[5,3]E[6,3]D[5,4]E[5,3]D[0,6]E[1,6]D[0,5]E[1,5]D[0,4]E[1,4]D[6,3]E[7,3]D[1,6]E[2,6]D[1,5]E[4,5]D[3,6]E[3,5]D[2,6]E[3,6]D[6,4]E[6,5]D[1,4]E[2,4]D[7,3]E[8,3]D[3,6]E[4,6]D[3,5]E[3,4]D[5,7]E[6,7]D[5,6]E[5,8]D[3,7]E[4,7]D[4,5]E[5,5]D[9,5]E[8,5][8,6]D[5,5]E[5,4][6,4]D[8,6]E[7,6][7,7]D[4,4]E[4,3][3,3]D[6,4]E[7,4][7,3]D[7,7]E[7,8][6,8]D[4,7]E[5,7]D[3,4]E[4,4][4,3]D[4,6]E[5,6]D[0,7]E[1,7]D[6,7]E[7,7][7,8]D[6,5]E[7,5][7,6]D[6,6]E[6,5]D[7,6]E[7,7]D[6,5]E[7,5][7,4]D[7,7]E[8,7]D[2,4]E[3,4]D[8,4]E[9,4][9,3]D[7,4]E[8,4]D[8,3]E[8,2]D[8,4]E[8,3]D[8,2]E[9,2]D[7,3]E[7,2][8,2]D[4,3]E[4,2][5,2]D[7,8]E[7,9][8,9]D[6,8]E[6,9][5,9]D[5,6]E[6,6][6,8]D[1,7]E[2,7]D[8,7]E[9,7]D[5,7]E[6,7]D[0,3]E[0,4]D[1,3]E[0,3]D[0,4]E[2,4]D[9,7]E[10,7][10,9][9,9]D[6,7]E[7,7][7,9][6,9]D[2,4]E[2,5][5,5][5,4][6,4][6,3]D[1,8]E[1,9]D[0,8]E[1,8][1,7]D[1,9]E[1,8][0,8]D[1,7]E[1,9][3,9]D[2,7]E[1,7][1,9][2,9]D[3,3]E[4,3]D[3,4]E[3,3]D[4,3]E[4,1][2,1][2,2]D'
 
 class PieceMover:
@@ -16,7 +21,7 @@ class PieceMover:
 
 
 def excuteCommand(command, pieceMover, mode="real"):
-    """
+    
     This function takes a command in string form that is read and then excuted by
     the pico causing the robotic assembly to move.
     Args:
@@ -29,7 +34,7 @@ def excuteCommand(command, pieceMover, mode="real"):
         location. Real will move the robotic assembly.
     Returns:
 
-    """
+    
     # The mode is fake so we just bypass the function.
     if mode == "fake":
         print("Mode is fake. This message is to signify a mock moving of the pieces. Returning from function!")
@@ -68,7 +73,7 @@ def excuteCommand(command, pieceMover, mode="real"):
 pieceMover = PieceMover()
 excuteCommand(command=testStr, pieceMover=pieceMover)
 
-"""
+
 import chess
 from chess_adversarial_search.game_manager import Game
 from chess_adversarial_search.minimax_alpha_beta import timeBoundedMinimaxAlphaBetaSearch
