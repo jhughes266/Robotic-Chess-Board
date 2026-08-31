7a85dd78e49ca77823b8c0ebc629dbfd9a4c3f48
Jotham Hughes <jhughes266@outlook.com>
2026-09-01 07:09:23 +1000

    Adding comments and documentation to the code

--- Rpi Scripts/chess_adversarial_search/game.py
+++ Rpi Scripts/chess_adversarial_search/game.py
@@ -6,44 +6,87 @@ from chess_adversarial_search.misc import Timer
 import copy
 
 class Game:
-    # Class vars
+    """
+    Implements the game class that is used by the alpha beta minimax algorithm. Contains all the neccessary methods to
+    enable the searching of the game tree.
+    """
+    # Class vars. Representing the stage of the game
     EARLY_MIDDLE_GAME = 0
     END_GAME = 1
 
     def __init__(self, boardManager):
-        self.__pieceSquareTables = PieceSquareTables()
+        """
+        Initializes the game object.
+        Args:
+            boardManager: A board manager object. Enables instances of the game class to communicate with the board.
+            It sole purpose in this class is to enable the game to check if a promotion is legal.
+        """
         self.__boardManager = boardManager
+        # Initiate the piece square tables
+        self.__pieceSquareTables = PieceSquareTables()
 
     def toMove(self, chessBoard):
+        """
+        Gets the player whos turn it is.
+        Args:
+            chessBoard: A python chess board object.
+        Returns:
+            A boolean indicating the player whos turn it is.
+        """
         return chessBoard.turn
 
-    def promotionIsIlegal(self, move):
+    def promotionIsIllegal(self, move):
+        """
+        Determines if a promotion is legal. Legal in this sense means if it can be supported by the board. For example
+        the player may want a bishop but there may not be any in the piece grave yard. In this case the function would
+        return True because the promotion is legal.
+        Args:
+             move: A python chess move object.
+        Returns:
+            A boolean indicating if the promotion is ILLEGAL. True means that the promotion is illegal and False means
+            that the promotion is legal.
+        """
+        # Only need to call the board managers method if a promotion is actually taking place.
         if move.promotion is not None:
             return self.__boardManager.promotionIsIllegal(move)
         return False
 
     def actions(self, chessBoard):
-        # May need to adapt this for the promotion if material is not available
+        """
+        Gets the available actions (moves) given the current state of the board. Also implements move ordering.
+        Args:
+            chessBoard: A python chess board object.
+        Returns:
+            A list of ordered legal moves that have been order according to a custom moving ordering algorithm
+        """
+        # Get all the available legal moves. At this stage they are not ordered
         unorderedLegalMoves = list(chessBoard.legal_moves)
-        # HERE IS WHERE PROMOTIONS WOULD BE FILTERED OUT
-        
+
+        # Seperate lists to store all the different kinds of moves
         captures = []
         threats = []
         forwardMoves = []
         remainingMoves = []
 
+        # There is varying procedures for each colour at same point could probably combine these into one and parameterize
+        # the players turn.
         if chessBoard.turn == chess.WHITE:
             for move in unorderedLegalMoves:
                 # Checks if the move is a promotion and if its illegal (The piece that is being promoted isnt in the
                 # graveyard). Then we dont add it to the list and just continue and move onto the next move.
-                if self.promotionIsIlegal(move):
+                if self.promotionIsIllegal(move):
                     continue
+                # The square the move is coming from
                 originSquare = move.from_square
+                # The square that the move is going too
                 destinationSquare = move.to_square
 
                 # Check for captures
+                # Get a bit mask representation of the destination square
                 destinationSquareBitMask = (1 << destinationSquare)
+                # Get a bit mask of all the square that are occupied by black PRIOR to the move.
                 occupiedByBlackBitMask = chessBoard.occupied_co[chess.BLACK]
+                # If the destination is occupied by black it means white is capturing.
                 if occupiedByBlackBitMask & destinationSquareBitMask:
                     captures.append(move)
                     continue
@@ -68,70 +111,93 @@ class Game:
 
         elif chessBoard.turn == chess.BLACK:
             for move in unorderedLegalMoves:
-                # Checks if the move is a promotion and if its illegal (The piece that is being promoted isnt in the
-                # graveyard). Then we dont add it to the list and just continue and move onto the next move.
-                if self.promotionIsIlegal(move):
+
+                if self.promotionIsIllegal(move):
                     continue
                 originSquare = move.from_square
                 destinationSquare = move.to_square
 
-                # Check for captures
                 destinationSquareBitMask = (1 << destinationSquare)
                 occupiedByWhiteBitMask = chessBoard.occupied_co[chess.WHITE]
                 if occupiedByWhiteBitMask & destinationSquareBitMask:
                     captures.append(move)
                     continue
 
-                # Check for threats
-                # A move is potentially mitigating a threat if the moves origin is being attacked. We can use the
-                # attackers function to check for attackers from the origin square of the move. If there is any the
-                # bit mask will contain 1's and be greater than 0 and evaluate true. If there is none it will equal 0.
                 if int(chessBoard.attackers(chess.WHITE, originSquare)):
                     threats.append(move)
                     continue
 
-                # Check for forward moves.
                 originRank = chess.square_rank(originSquare)
                 destinationRank = chess.square_rank(destinationSquare)
                 if destinationRank <= originRank:
                     forwardMoves.append(move)
                     continue
 
-                # Append remaining moves.
                 remainingMoves.append(move)
 
-        # Concat lists
+        # Concat lists in the order of moves desired.
         orderedLegalMoves = captures + threats + forwardMoves + remainingMoves
-        #print(len(unorderedLegalMoves)==len(orderedLegalMoves))
 
+        # Return an order list of legal moves.
         return orderedLegalMoves
 
 
     def result(self, chessBoard, move):
+        """
+        Executes a move on the python chess board.
+        Args:
+            chessBoard: A python chess board object.
+            move: A python chess move object.
+        Returns:
+            An updated python board object with the new move pushed onto it.
+        """
         chessBoard.push(move)
         return chessBoard
 
     def reverseMove(self, chessBoard):
+        """
+        Reverses the last move made.
+        Args:
+            chessBoard: A python chess board object.
+        Returns:
+            A old version of the chess board prior to the last move made(dont really need to return this).'
+        """
         return chessBoard.pop()
 
     def isTerminal(self, chessBoard):
-        ## This may need expanding ##
+        """
+        Checks if the game is over.
+        Args:
+            chessBoard: A python chess board object.
+        Returns:
+            A boolean indicating if the game is over. True if the game is over, False if it is not.
+        """
         return chessBoard.is_game_over()
 
     def utility(self, chessBoard, plyDepth):
+        """
+        Determines the utility of the given chess board at a TERMINAL game state.
+        Args:
+             chessBoard: A python chess board object.
+             plyDepth: The depth of the current search ply.
+        Returns:
+            Utility of the terminal position.
+        """
+        # Get the result string of the game.
         resultStr = chessBoard.result()
-        # For the time being a draw is assigned a zero (this may need changing)
+        # For the time being a draw is assigned a zero.
         if resultStr == '1/2-1/2':
             return 0
         # We now know that the score is either '1-0' or '0-1'
         elif resultStr == '1-0':
-            # White wins
+            # White wins. Subtracting the ply depth means earlier checkmates are prioritised. Prevents the bot from
+            # constantly postponing the checkmate and getting caught in a loop.
             return 999999 - plyDepth
         elif resultStr == '0-1':
             # Black wins
             return -999999 + plyDepth
         else:
-            raise RuntimeError("Attempted to calculate utility but the game was not in a terminal state!")
+            assert False, "The game was not in a terminal game state! This should not happen inside this function."
 
     def evaluate(self, chessBoard):
         materialValue = 0
