d8d324d1d43d46dc53a89558ebe8b203e374f560
Jotham Hughes <jhughes266@outlook.com>
2026-09-21 07:42:47 +1000

    Working on the keypoint triangulator. The main thing that was worked on here was getting the hand landmark detection working and also getting a system that access both the usb and csi cameras

---
+++ Rpi Scripts/managers/game_manager/user_interface/keypoint_triangulation/camera.py
@@ -0,0 +1,83 @@
+from abc import ABC, abstractmethod
+
+from console_debug_info import ConsoleDebugInfo
+import cv2
+
+
+class camera(ABC):
+    def __init__(self, deviceIndex=0):
+        self._deviceIndex = deviceIndex
+
+    @abstractmethod
+    def setUp(self):
+        pass
+
+    @abstractmethod
+    def tearDown(self):
+        pass
+
+    @abstractmethod
+    def captureImage(self):
+        pass
+
+class OpenCvDevice(camera):
+    def setUp(self):
+        self.__cap = cv2.VideoCapture(self._deviceIndex)
+        if not self.__cap.isOpened():
+            ConsoleDebugInfo.printToConsole("Could not open camera")
+            exit(0)
+
+    def tearDown(self):
+        self.__cap.release()
+        cv2.destroyAllWindows()
+        del self.__cap
+
+    def captureImage(self):
+        ret, frame = self.__cap.read()
+        if ret:
+            return frame
+
+        ConsoleDebugInfo.printToConsole("Could not capture image with the OPENCV device!")
+        return None
+
+class PiCameraDevice(camera):
+    def setUp(self):
+        self.__piCamera = DummyPicamera2(self._deviceIndex)
+        self.__piCamera.start()
+
+    def tearDown(self):
+        self.__piCamera.stop_preview()
+        self.__piCamera.stop()
+        self.__piCamera.close()
+        del self.__piCamera
+
+    def captureImage(self):
+        array = None
+        try:
+            array = self.__piCamera.capture_array()
+        except Exception as e:
+            ConsoleDebugInfo.printToConsole("Could not capture image with the PICAMERA device!")
+
+        return array
+
+class DummyPicamera2():
+    def __init__(self, deviceIndex):
+        ConsoleDebugInfo.printToConsole("INITIATING A DUMMY PICAMERA DEVICE PLEASE USE THE REAL PICAMERA IN THE ACTUAL SCRIPT")
+
+    def start(self):
+        ConsoleDebugInfo.printToConsole("STARTING DUMMY PICAMERA")
+
+    def stop_preview(self):
+        ConsoleDebugInfo.printToConsole("STOPPING PREVIEW FOR DUMMY PICAMERA")
+
+    def stop(self):
+        ConsoleDebugInfo.printToConsole("STOPPING DUMMY PICAMERA")
+
+    def close(self):
+        ConsoleDebugInfo.printToConsole("CLOSING DUMMY PICAMERA")
+
+    def capture_array(self):
+        #ConsoleDebugInfo.printToConsole(f"CAPTURING A DUMMY PICAMERA IMAGE")
+        return None
+
+
--- Rpi Scripts/managers/game_manager/user_interface/keypoint_triangulation/keypoint_triangulator.py
+++ Rpi Scripts/managers/game_manager/user_interface/keypoint_triangulation/keypoint_triangulator.py
@@ -1,44 +1,137 @@
+from operator import iadd
+
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import Image, ImageFormat
 import matplotlib.pyplot as plt
 import numpy as np
+from mediapipe.tasks.python.vision.drawing_utils import draw_landmarks
+
+from managers.game_manager.user_interface.keypoint_triangulation.camera import *
 
 class KeypointTriangulator:
-    def __init__(self, refCamIntrinsicDir, offsetCamIntrinsicDir):
-        self.__refCameraMatrix = np.loadtxt(refCamIntrinsicDir + '/camera_matrix.txt')
-        self.__offsetCameraMatrix = np.loadtxt(offsetCamIntrinsicDir + '/camera_matrix.txt')
+    def __init__(self, aCamObj, bCamObj, aCamIntrinsicDir, bCamIntrinsicDir):
+        self.__aCamObj = aCamObj
+        self.__bCamObj = bCamObj
 
-        self.__refDistCoeffs = np.loadtxt(refCamIntrinsicDir + '/dist.txt')
-        self.__offsetDistCoeffs = np.loadtxt(offsetCamIntrinsicDir + '/dist.txt')
+        self.__aCamMatrix = np.loadtxt(aCamIntrinsicDir + '/camera_matrix.txt')
+        self.__bCamMatrix = np.loadtxt(bCamIntrinsicDir + '/camera_matrix.txt')
+
+        self.__aDistCoeffs = np.loadtxt(aCamIntrinsicDir + '/dist.txt')
+        self.__bDistCoeffs = np.loadtxt(bCamIntrinsicDir + '/dist.txt')
+
+    def setUp(self):
+        self.__aCamObj.setUp()
+        self.__bCamObj.setUp()
+        base_options = python.BaseOptions(model_asset_path='resources/hands_model/hand_landmarker.task')
+        options = vision.HandLandmarkerOptions(base_options=base_options,
+                                               min_hand_detection_confidence=0.8,
+                                               min_hand_presence_confidence=0.8,
+                                               min_tracking_confidence=0.8,
+                                               num_hands=1)
+        self.__detector = vision.HandLandmarker.create_from_options(options=options)
+
+    def tearDown(self):
+        self.__aCamObj.tearDown()
+        self.__bCamObj.tearDown()
 
     def run(self):
-        refCameraImage, offsetCameraImage = self.__captureImages()
+        aCamImage, bCamImage = self.__captureImages()
 
-        refCameraImageUndistorted, newRefCameraMatrix = self.__undistortImage(refCameraImage, self.__refCameraMatrix, self.__offsetCameraMatrix)
-        offsetCameraImageUndistorted, newOffsetCameraMatrix = self.__undistortImage(refCameraImage, self.__offsetCameraMatrix, self.__offsetCameraMatrix)
+        aCamImageUndistorted, aNewCamMatrix = self.__undistortImage(aCamImage, self.__aCamMatrix, self.__aDistCoeffs)
+        bCamImageUndistorted, bNewCamMatrix = self.__undistortImage(bCamImage, self.__bCamMatrix, self.__bDistCoeffs)
 
-        self.__keypointExtraction()
+        aCamDetectionResult, bCamDetectionResult = self.__keypointExtraction(aCamImageUndistorted, bCamImageUndistorted)
 
-        self.__triangulation()
+        ### For debugging ####
+        if aCamDetectionResult != None or bCamDetectionResult != None:
+            self.__drawKeypoints(aCamDetectionResult, aCamImage)
+            self.__drawKeypoints(bCamDetectionResult, bCamImage)
+
+        combinedImage = np.hstack((aCamImage, bCamImage))
+
+        cv2.imshow('capture', combinedImage)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return False
+        ######################
+
+        self.__triangulePoints(aCamDetectionResult=aCamDetectionResult, bCamDetectionResult=bCamDetectionResult,
+                               aNewCamMatrix=aNewCamMatrix , bNewCamMatrix=bNewCamMatrix, offset=(0,0,0))
+        return True
 
     def __captureImages(self):
-        refCamImage = None
-        offsetCamImage = None
-        return refCamImage, offsetCamImage
+        aCamImage = self.__aCamObj.captureImage()
+        bCamImage = self.__bCamObj.captureImage()
 
-    def __undistortImage(self, image, cameraMatrix, distCoeffs):
+        return aCamImage, bCamImage
+
+    def __undistortImage(self, image, camMatrix, distCoeffs):
         h, w = image.shape[:2]
-        newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, (w, h), 1, (w, h))
-        undistorted = cv2.undistort(image, cameraMatrix, distCoeffs, None, newCameraMatrix)
+        newCamMatrix, roi = cv2.getOptimalNewCameraMatrix(camMatrix, distCoeffs, (w, h), 1, (w, h))
+        undistorted = cv2.undistort(image, camMatrix, distCoeffs, None, newCamMatrix)
         x, y, w, h = roi
         croppedUndistorted = undistorted[y:y + h, x:x + w]
-        return croppedUndistorted, newCameraMatrix
+        return croppedUndistorted, newCamMatrix
 
-    def __keypointExtraction(self):
-        pass
+    def __keypointExtraction(self, aCamImage, bCamImage):
+        aCamImageRgb = cv2.cvtColor(aCamImage, cv2.COLOR_BGR2RGB)
+        bCamImageRgb = cv2.cvtColor(bCamImage, cv2.COLOR_BGR2RGB)
 
-    def __triangulation(uRefCam, vRefCam, uOffsetCam, vOffsetCam, refCameraMatrix, offsetCameraMatrix, xOffset, yOffset, zOffset):
+        aCamMpImage = mp.Image(image_format=mp.ImageFormat.SRGB, data=aCamImageRgb)
+        bCamMpImage = mp.Image(image_format=mp.ImageFormat.SRGB, data=bCamImageRgb)
 
-        print(offsetCameraMatrix)
+        aCamDetectionResult = self.__detector.detect(aCamMpImage)
+        bCamDetectionResult = self.__detector.detect(bCamMpImage)
+
+        if len(aCamDetectionResult.hand_landmarks) == 0 or len(bCamDetectionResult.hand_landmarks) == 0:
+            return None, None
+
+        # Put the x and y coordinates in pixels
+        aCamDetectionResult = self.__normalizedCordinatesToPixel(results=aCamDetectionResult,
+                                                                 xDim=aCamImage.shape[1],
+                                                                 yDim=bCamImage.shape[0])
+
+        bCamDetectionResult = self.__normalizedCordinatesToPixel(results=bCamDetectionResult,
+                                                                   xDim=aCamImage.shape[1],
+                                                                   yDim=bCamImage.shape[0])
+
+
+
+        return aCamDetectionResult, bCamDetectionResult
+
+    def __triangulePoints(self, aCamDetectionResult, bCamDetectionResult , aNewCamMatrix, bNewCamMatrix, offset):
+        xOffset = offset[0]
+        yOffset = offset[1]
+        zOffset = offset[2]
+
+        fxa = aNewCamMatrix[0][0]
+        fya = aNewCamMatrix[1][1]
+        oxa = aNewCamMatrix[0][2]
+        oxb = aNewCamMatrix[1][2]
+
+        fxb = bNewCamMatrix[0][0]
+        fyb = bNewCamMatrix[1][1]
+        oxb = bNewCamMatrix[0][2]
+        oxb = bNewCamMatrix[1][2]
+
+        for aCamLandmark, bCamLandmark in zip(aCamDetectionResult.hand_landmarks[0], bCamDetectionResult.hand_landmarks[0]):
+
+
+        """
+        ia = 
+        ja =
+        ka =
+
+        ib =
+        jb =
+        kb =
+        
+        
+        
+        print(bCamMatrix)
         fig = plt.figure()
         ax = plt.axes(projection='3d')
 
@@ -52,7 +145,31 @@ class KeypointTriangulator:
         y = (z * (vRefCam - oy)) / fy
         ax.plot3D(x, y, z, 'red')
         plt.show()
+        """
 
-test = KeypointTriangulator(refCamIntrinsicDir='resources/calibration_results/usb_cam', offsetCamIntrinsicDir='resources/calibration_results/csi_cam')
+    def __normalizedCordinatesToPixel(self, results, xDim, yDim):
+        landmarkList = results.hand_landmarks[0]
+        for landmark in landmarkList:
+            landmark.x = landmark.x * xDim
+            landmark.y = landmark.y * yDim
+        return results
+
+    def __drawKeypoints(self, results, image):
+        hand_landmarks_list = results.hand_landmarks
+        #print(results.handedness)
+        if len(hand_landmarks_list) > 0:
+            for hand in hand_landmarks_list:
+                for landmark in hand:
+                    cv2.circle(image, (int(landmark.x), int(landmark.y)), 5, (0, 0, 255), -1)
+
+testCam1 = OpenCvDevice(0)
+#testCam2 = OpenCvDevice(0)
+testTri = KeypointTriangulator(aCamObj=testCam1, bCamObj=testCam1,aCamIntrinsicDir='resources/calibration_results/usb_cam', bCamIntrinsicDir='resources/calibration_results/csi_cam')
+testTri.setUp()
+
+while testTri.run():
+    pass
+
+testTri.tearDown()
 
 
