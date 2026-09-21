f062f091170f7e5ea265ebaea8d15706170d0e40
Jotham Hughes <jhughes266@outlook.com>
2026-09-21 17:56:47 +1000

    Finished the camera calibration imager taking script and took images

--- Rpi Scripts/managers/game_manager/user_interface/keypoint_triangulation/calibration_image_taker.py
+++ Rpi Scripts/managers/game_manager/user_interface/keypoint_triangulation/calibration_image_taker.py
@@ -1,21 +1,25 @@
 from managers.game_manager.user_interface.keypoint_triangulation.camera import *
+#from camera import *
 
 import numpy as np
 
-usbCam = OpenCvDevice(0)
-csiCam = PiCameraDevice(0)
+usbCam = OpenCvDevice(captureWidth=640, captureHeight=480, deviceIndex = 0)
+csiCam = PiCameraDevice(captureWidth=640, captureHeight=480, deviceIndex = 0)
 
 usbCam.setUp()
 csiCam.setUp()
 
-usbImage = usbCam.captureImage()
-csiImage = usbCam.captureImage()
+numCalibrationImages = 20
+for i in range(numCalibrationImages):
+    input("Hit enter to take next image")
+    usbImage = usbCam.captureImage()
+    csiImage = csiCam.captureImage()
 
-combinedImage = np.hstack((csiImage, csiImage))
-cv2.imshow('capture', combinedImage)
-if cv2.waitKey(0) & 0xFF == ord('q'):
-    pass
+    combinedImage = np.hstack((csiImage, usbImage))
+    cv2.imshow('capture', combinedImage)
+    
+    cv2.imwrite(f"resources/calibration_images/usb_cam/cal_{i}.jpg",usbImage)
+    cv2.imwrite(f"resources/calibration_images/csi_cam/cal_{i}.jpg", csiImage)
 
 usbCam.tearDown()
 csiCam.tearDown()
-
